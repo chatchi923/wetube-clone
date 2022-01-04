d707d6cc14f4991c388389513377b4096797af72
Jeewon Choi <chatchi923@gmail.com>
2022-01-04 00:47:23 -0700

    3.0

--- index.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello Node JS");
-
-const express = require("express");
-const app = express();
---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () =>
+  console.log(`✅ Server listening on port https://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
