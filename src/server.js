afbd474c1bea45fa35249923ec99708cafc48a93
Jeewon Choi <chatchi923@gmail.com>
2022-01-04 01:09:05 -0700

    3.2

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`✅ Server listening on port https://localhost:${PORT} 🚀`);
 
