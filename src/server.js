627b132b7224c27e3d1de1f48ee42e873a118158
Jeewon Choi <chatchi923@gmail.com>
2022-01-05 13:28:39 -0700

    3.6

--- src/server.js
+++ src/server.js
@@ -4,16 +4,24 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
 
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed. You may continue");
+  next();
+};
 const handleHome = (req, res) => {
-  return res.end();
+  return res.send("I love middlewares.");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+app.get("/", logger, handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port https://localhost:${PORT} 🚀`);
