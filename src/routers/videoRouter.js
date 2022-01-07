431fd138a3e60878aa971b573a63b9a2123872e7
Jeewon Choi <chatchi923@gmail.com>
2022-01-07 10:24:49 -0700

    4.7

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,6 +4,7 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
+/* /upload should come ahead of /:id */
 videoRouter.get("/:id", see);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", deleteVideo);
