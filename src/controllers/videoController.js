d48eb3ae1bc3c2d0f328ef795ab28979f7d45d84
Jeewon Choi <chatchi923@gmail.com>
2022-01-16 00:12:33 -0700

    6.15

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -18,10 +18,20 @@ export const postEdit = (req, res) => {
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
-  res.render("upload", { pageTitle: "Upload Video" });
+  return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = (req, res) => {
-  const { title } = req.body;
-  console.log(req.body);
-  res.redirect("/");
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  console.log(video);
+  return res.redirect("/");
 };
